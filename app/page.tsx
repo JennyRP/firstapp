053700f2173e4bfd6f65f32f591203b37b19b7c1
Jenny <jr3342992@gmail.com>
2024-02-13 14:33:57 -0600

    primero

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "@/styles/index.module.css"
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.container}>
+        <nav className={styles.navbar}>
+          <a href="#">Home</a>
+          <a href="#">Acerca</a>
+        </nav>
+      </div>
+    </main>
+  );
+}
