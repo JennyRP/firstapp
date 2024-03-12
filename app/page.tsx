1c670cd55ffea7a7c4a94e35c5f9946faa6f9a20
JennyRP <147959956+JennyRP@users.noreply.github.com>
2024-03-11 19:42:13 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "@/styles/index.module.css"
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -7,9 +7,21 @@ export default function Home() {
       <div className={styles.container}>
         <nav className={styles.navbar}>
           <a href="#">Home</a>
-          <a href="#">Acerca</a>
+          <a href="#">Acerca de</a>
         </nav>
+
+        <div className="flex justify-center items-center h-screen">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold mb-4">Rivera Paz Jenny TI03SM-22</h2>
+            <Image
+              src="https://albumizr.com/ia/e9cce529d1c5451ab5d556cb70bc2d7c.jpg"
+              alt="Paisaje del videojuego The legend of Zelda: Breath of the wild "
+              width={400}
+              height={300} // Agrega la propiedad height aquí
+            />
+          </div>
+        </div>
       </div>
-    </main>
-  );
+    </main>
+  );
 }
